99a2d52c0a7022e6e4c3407b8c1ad276a4f921b5
Miikka Koskinen <miikka.koskinen@iki.fi>
2014-09-13 15:23:43 +0300

    Add navigation bar

--- src/kahvipaivakirja/views.clj
+++ src/kahvipaivakirja/views.clj
@@ -4,22 +4,52 @@
    [clojure.java.io :as io]
    [endophile.core :refer [mp to-clj html-string]]
    [hiccup.core :refer [html]]
-   [hiccup.page :refer [include-css]]))
+   [hiccup.element :refer [link-to]]
+   [hiccup.page :refer [html5 include-css include-js]]))
 
 (defn ^:private include-bootstrap []
-  (include-css "/bootstrap/css/bootstrap.css"
-               "/bootstrap/css/bootstrap-theme.css"))
+  (list (include-css "/bootstrap/css/bootstrap.css"
+                     "/bootstrap/css/bootstrap-theme.css")
+        (include-js "/bootstrap/js/bootstrap.js")))
 
-(defn front-page []
-  (html
+(defn ^:private active?
+  [page current & attr-map]
+  (if (= page current)
+    (merge {:class "active"} attr-map)
+    attr-map))
+
+(defn ^:private base [page title & content]
+  (html5 {:lang "fi"}
    [:head
-    [:title "Kahvipäiväkirja"]
+    [:title (if title
+              (str title " - Kahvipäiväkirja")
+              "Kahvipäiväkirja")]
+    [:meta {:charset "utf-8"}]
+    [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
     (include-bootstrap)]
    [:body
-    [:h1 "Tervetuloa Kahvipäiväkirjaan!"]]))
+    [:nav.navbar.navbar-default {:role "navigation"}
+     [:div.container-fluid
+      [:div.navbar-header
+       [:button {:type "button" :class "navbar-toggle collapsed"
+                 :data-toggle "navbar"}
+        [:span.sr-only "Toggle navigation"]
+        [:span.icon-bar]
+        [:span.icon-bar]
+        [:span.icon-bar]]
+       [:a.navbar-brand {:href "#"} "Kahvipäiväkirja"]]
+      [:div#navbar {:class "collapse navbar-collapse"}
+       [:ul.nav.navbar-nav
+        [:li (active? :front-page page) (link-to "/" "Etusivu")]]]]]
+    [:div.container content]]))
 
-(defn readme []
+(defn front-page []
+  (base :front-page "Etusivu"
+   [:h1 "Tervetuloa kahvipäiväkirjaan"]))
+
+(defn readme
   "Render README.md as HTML."
+  []
   (let [content (-> (io/resource "README.md")
                     (io/file)
                     (slurp)
